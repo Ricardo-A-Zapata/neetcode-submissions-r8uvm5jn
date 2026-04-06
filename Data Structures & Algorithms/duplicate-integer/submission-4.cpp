b17d71299f5a59eabf81ca528ac902d437a6ff18
Ricardo-A-Zapata <96500107+Ricardo-A-Zapata@users.noreply.github.com>
2026-04-06 15:16:58 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::map<int, int> dict;
+
+        for (const int num: nums){
+            if (dict.count(num) == 0){
+                dict[num] = 1;
+            } else return true;
+        }
+        return false;
+        
+    }
+};
